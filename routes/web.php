14b7852b1d77a856e96eefcc9357046f96143a81
hoàng tân <hoangnhattan2k2@gmail.com>
2022-09-24 22:41:52 +0700

    make it better

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('chinh-sach-phat-trien', function(){
+    return  " <h1>Chính sách phát triển  </h1> ";
+});
+
+Route::get('auth/facebook' , function(){
+    return Socialite::driver('facebook')->redirect();
+});
+
+Route::get('/auth/facebook/callback', function () {
+    $user = Socialite::driver('facebook')->user();
+ 
+    dd($user);
+});
